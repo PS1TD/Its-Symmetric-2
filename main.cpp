64b89cc9618c9c78649a3fbac3edda12e365f2c4
Alexander Vorobyev <vent1narc@gmail.com>
2021-07-04 01:26:55 +0300

    💢 Symmetry Checks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <math.h>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -11,6 +12,129 @@ struct cell {
 	int value;
 	bool colored;
 };
+
+bool isHorizontallySymmetric(vector<vector<cell>> &grid) {
+	for (size_t i = 0; i < ceil(grid.size() / 2.0); i++) {
+		for (size_t j = 0; j < grid.size(); j++) {
+			if (grid[i][j].colored != grid[grid.size() - i - 1][j].colored) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool isVerticallySymmetric(vector<vector<cell>> &grid) {
+	for (size_t i = 0; i < grid.size(); i++) {
+		for (size_t j = 0; j < ceil(grid.size() / 2.0); j++) {
+			if (grid[i][j].colored != grid[i][grid.size() - j - 1].colored) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool isTLtoBRDiagonallySymmetric(vector<vector<cell>> &grid) {
+
+	size_t j = 0;
+	for (size_t i = 0; i < grid.size(); i++) {
+		for (size_t k = j; k < grid.size(); k++) {
+			if (grid[i][k].colored != grid[k][i].colored) {
+				return false;
+			}
+		}
+		j++;
+	}
+	return true;
+}
+
+bool isBLtoTRDiagonallySymmetric(vector<vector<cell>> &grid) {
+
+	size_t j = grid.size() - 1;
+	for (size_t i = 0; i < grid.size(); i++) {
+		for (int k = j; k >= 0; k--) {
+			if (grid[i][k].colored !=
+				grid[grid.size() - 1 - k][grid.size() - 1 - i].colored) {
+				return false;
+			}
+		}
+		j--;
+	}
+	return true;
+}
+
+bool isRotationallySymmetric(vector<vector<cell>> &grid) {
+	for (size_t i = 0; i < ceil(grid.size() / 2.0); i++) {
+		for (size_t j = 0; j < ceil(grid.size() / 2.0); j++) {
+			// 90
+			// 0 0 -> 0 4
+			// 0 1 -> 1 4
+			// 0 2 -> 2 4
+			// 1 0 -> 0 3
+			// 1 1 -> 1 3
+			// 1 2 -> 2 3
+			// 2 0 -> 0 2
+			// 2 1 -> 1 2
+			// 2 2 -> 2 2
+			if (grid[i][j].colored != grid[j][grid.size() - i - 1].colored) {
+				return false;
+			}
+
+			// 180
+			// 0 0 -> 4 4
+			// 0 1 -> 4 3
+			// 0 2 -> 4 2
+			// 1 0 -> 3 4
+			// 1 1 -> 3 3
+			// 1 2 -> 3 2
+			// 2 0 -> 2 4
+			// 2 1 -> 2 3
+			// 2 2 -> 2 2
+			if (grid[i][j].colored !=
+				grid[grid.size() - i - 1][grid.size() - j - 1].colored) {
+				return false;
+			}
+
+			// 270
+			// 0 0 -> 4 0
+			// 0 1 -> 3 0
+			// 0 2 -> 2 0
+			// 1 0 -> 4 1
+			// 1 1 -> 3 1
+			// 1 2 -> 2 1
+			// 2 0 -> 4 2
+			// 2 1 -> 3 2
+			// 2 2 -> 2 2
+			if (grid[i][j].colored != grid[grid.size() - j - 1][i].colored) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool isSymmetric(vector<vector<cell>> &grid) {
+	bool h = isHorizontallySymmetric(grid);
+	cout << "Horizontally Symmetric: " << h << endl;
+
+	bool v = isVerticallySymmetric(grid);
+	cout << "Vertically Symmetric: " << v << endl;
+
+	bool tlbr = isTLtoBRDiagonallySymmetric(grid);
+	cout << "Top Left to Bottom Right Symmetric: " << tlbr << endl;
+
+	bool bltr = isBLtoTRDiagonallySymmetric(grid);
+	cout << "Bottom Left to Top Right Symmetric: " << bltr << endl;
+
+	bool r = isRotationallySymmetric(grid);
+	cout << "Rotationally Symmetric: " << r << endl;
+
+	bool a = h || v || tlbr || bltr || r;
+	cout << "Any Symmetric: " << a << endl;
+
+	return a;
+}
 
 size_t getLargestCellSize(vector<vector<cell>> &grid) {
 	size_t maxSize = 0;
@@ -126,6 +250,8 @@ int main() {
 		}
 
 		printGrid(grid);
+
+		isSymmetric(grid);
 
 		inputfile.close();
 	} else {
